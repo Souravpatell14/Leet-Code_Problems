21b95f4183edb637d99931a9059379e53cb246e3
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-01-22 05:42:12 -0500

    Added happy number solution

---
+++ 202_Happy_Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int squareSum(int n) {
+        int sum=0;
+        while (n>0) {
+            int d=n%10;
+            sum+=d*d;
+            n/=10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do {
+            slow=squareSum(slow);                 // 1 step
+            fast=squareSum(squareSum(fast));     // 2 steps
+        } while(slow != fast);
+
+        return slow==1;
+    }
+};
