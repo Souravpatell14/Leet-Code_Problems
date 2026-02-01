075e0eb3dfcdbc8ccf28da1f276eb553e76b5132
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-02-01 09:16:55 -0500

    Added a new problem

---
+++ (01) Easy Level Problem/283_Move_Zero.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;  // next position for non-zero
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
+                if (i != pos) {
+                    nums[i] = 0;
+                }
+                pos++;
+            }
+        }
+    }
+};
