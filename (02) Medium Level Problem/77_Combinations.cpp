4c5ef06cb94aa82bbd70451283f97b96a9dd17dc
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-04-23 15:12:33 -0500

    Added a new problems

---
+++ (02) Medium Level Problem/77_Combinations.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    void solve(int start, int n, int k, vector<int>& temp) {
+
+        if (temp.size() == k) {
+            result.push_back(temp);
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            temp.push_back(i);      
+            solve(i + 1, n, k, temp);   
+            temp.pop_back();             
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        solve(1, n, k, temp);
+        return result;
+    }
+};
