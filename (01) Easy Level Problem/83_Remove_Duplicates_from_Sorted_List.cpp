b51b87d45a273b1fa841cd9702c2ea727a3aa178
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-04-26 12:40:59 -0500

    Added  problem no. 83

---
+++ (01) Easy Level Problem/83_Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* current = head;
+        while (current != NULL && current->next != NULL) {
+
+            if (current->val == current->next->val) {
+                current->next = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
